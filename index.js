995cfc48b78fd6a6635071a27c5da2e2219a358c
Mariia <mariia.bieskova@gmail.com>
2024-05-02 14:26:01 +0300

    set the score to increment properly and create a logic to annouce a winner

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice (){
     randomInt = Math.floor(Math.random() * 3)
     if (randomInt == 0)
@@ -8,64 +12,63 @@ function getComputerChoice (){
         return "rock";
 }
 
+function playRound(computerChoice, humanChoice){
+
+    if  (computerChoice == humanChoice){
+        console.log("It's a tie! Try again please.");
+    }    
+    else if (computerChoice == "paper" && humanChoice == "scissors"){
+        console.log("You win! Scissors beat paper.");
+        humanScore +=1;
+    }
+    else if (computerChoice == "rock" && humanChoice == "scissors"){
+        console.log("You loose! Rock beats scissors.");
+        computerScore +=1;
+    }
+    else if (computerChoice == "scissors" && humanChoice == "rock"){
+            console.log("You win! Rock beats scissors.");
+            humanScore +=1;
+    }
+    else if (computerChoice == "paper" && humanChoice == "rock"){
+        console.log("You win! Rock beats paper.");
+        humanScore +=1;
+    }
+    
+    else if (computerChoice == "scissors" && humanChoice == "paper"){
+            console.log("You loose! Scissors beat paper.");
+            computerScore +=1;
+    }
+    else {
+        console.log("You loose! Rock beats paper.");
+        computerScore += 1;
+    }   
+}
+
+
+// div.appendChild(msg);
+
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
         button.addEventListener("click", () => {
-            
-            function playGame (){
-                let humanScore = 0;
-                let computerScore = 0;
-                    
 
-                    for (i = 1; i < 6; i++){
+            const computerSelection = getComputerChoice();
+            const humanSelection = button.id;
                         
-                        const computerSelection = getComputerChoice();
-                        const humanSelection = button.id;
-                        
-                            function playRound(computerChoice, humanChoice){
-                    
-                                    if  (computerChoice == humanChoice){
-                                        console.log("It's a tie! Try again please.");
-                                    }    
-                                    else if (computerChoice == "paper" && humanChoice == "scissors"){
-                                        console.log("You win! Scissors beat paper.");
-                                        humanScore +=1;
-                                    }
-                                    else if (computerChoice == "rock" && humanChoice == "scissors"){
-                                        console.log("You loose! Rock beats scissors.");
-                                        computerScore +=1;
-                                    }
-                                    else if (computerChoice == "scissors" && humanChoice == "rock"){
-                                            console.log("You win! Rock beats scissors.");
-                                            humanScore +=1;
-                                    }
-                                    else if (computerChoice == "paper" && humanChoice == "rock"){
-                                        console.log("You win! Rock beats paper.");
-                                        humanScore +=1;
-                                    }
-                                    
-                                    else if (computerChoice == "scissors" && humanChoice == "paper"){
-                                            console.log("You loose! Scissors beat paper.");
-                                            computerScore +=1;
-                                    }
-                                    else {
-                                        console.log("You loose! Rock beats paper.");
-                                        computerScore += 1;
-                                    }   
-                                }
-                            
-                                    console.log("Computer selection:", computerSelection);
-                                    console.log("Your selection:", humanSelection);
-                                    playRound(computerSelection, humanSelection);
-                                    console.log("Your score:", humanScore);
-                                    console.log("Computer score:", computerScore); 
-                    
-                    }
-        }
-                        playGame();
-    })
-        
+            console.log("Computer selection:", computerSelection);
+            console.log("Your selection:", humanSelection);
+            playRound(computerSelection, humanSelection);
+            console.log("Your score:", humanScore);
+            console.log("Computer score:", computerScore); 
+            let msg = document.querySelector("#msg");
+
+            if (humanScore === 5){
+                msg.textContent = "Congratulations, you won!"
+            }
+            else if (computerScore === 5){
+                msg.textContent = "You lost! Try again later"
     
-});
-
+            }
+                    
+            })
+        });
