2e325d6f8db3f5efc18967b6b6cb40e9728efdea
Mariia <mariia.bieskova@gmail.com>
2024-05-02 14:55:41 +0300

    add paragraphs to print the game results to the screen

--- index.js
+++ index.js
@@ -12,55 +12,55 @@ function getComputerChoice (){
         return "rock";
 }
 
+const roundMsg = document.querySelector("#round-msg")
+
 function playRound(computerChoice, humanChoice){
 
     if  (computerChoice == humanChoice){
-        console.log("It's a tie! Try again please.");
+        roundMsg.textContent = "It's a tie! Try again please.";
     }    
     else if (computerChoice == "paper" && humanChoice == "scissors"){
-        console.log("You win! Scissors beat paper.");
+        roundMsg.textConten = "You win! Scissors beat paper.";
         humanScore +=1;
     }
     else if (computerChoice == "rock" && humanChoice == "scissors"){
-        console.log("You loose! Rock beats scissors.");
+        roundMsg.textContent = "You loose! Rock beats scissors.";
         computerScore +=1;
     }
     else if (computerChoice == "scissors" && humanChoice == "rock"){
-            console.log("You win! Rock beats scissors.");
+        roundMsg.textContent = "You win! Rock beats scissors.";
             humanScore +=1;
     }
     else if (computerChoice == "paper" && humanChoice == "rock"){
-        console.log("You win! Rock beats paper.");
+        roundMsg.textContent = "You win! Rock beats paper.";
         humanScore +=1;
     }
     
     else if (computerChoice == "scissors" && humanChoice == "paper"){
-            console.log("You loose! Scissors beat paper.");
+        roundMsg.textContent = "You loose! Scissors beat paper.";
             computerScore +=1;
     }
     else {
-        console.log("You loose! Rock beats paper.");
-        computerScore += 1;
+        roundMsg.textContent = "You win! Rock beats scissors.";
+        humanScore += 1;
     }   
 }
-
-
-// div.appendChild(msg);
 
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
         button.addEventListener("click", () => {
+        humanSelection = button.id;
 
-            const computerSelection = getComputerChoice();
-            const humanSelection = button.id;
+            const computerSelection = document.querySelector("#computer-choice");
+            computerSelection.textContent = `Computer selection: ${getComputerChoice()}`;
+            const playerScore = document.querySelector("#human-score");
+            playerScore.textContent = `Your score: ${humanScore + 1}`;
+            const computerPoints = document.querySelector("#computer-score");
+            computerPoints.textContent = `Computer score: ${computerScore}`;
                         
-            console.log("Computer selection:", computerSelection);
-            console.log("Your selection:", humanSelection);
             playRound(computerSelection, humanSelection);
-            console.log("Your score:", humanScore);
-            console.log("Computer score:", computerScore); 
-            let msg = document.querySelector("#msg");
+            const msg = document.querySelector("#game-msg");
 
             if (humanScore === 5){
                 msg.textContent = "Congratulations, you won!"
